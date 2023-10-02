93bb2576c48bb79ff578eb0764b5febf05b97cc0
Isabela Andrade <isabelaandradeee@hotmail.com>
2023-10-02 20:57:42 -0300

    ap11(login): campos login e senha

---
+++ src/main/java/br/com/isabela/mavenproject20232_sistemaacademico/Mavenproject20232_sistemaacademico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.isabela.mavenproject20232_sistemaacademico;
+
+/**
+ *
+ * @author 822137149
+ */
+public class Mavenproject20232_sistemaacademico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
